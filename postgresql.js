bf2568c1bc6b6eb6145c05b8bffe5e7d651c20ee
NgoMinhToan <toandev99@gmail.com>
2024-01-09 10:53:38 +0700

    add db save

--- postgresql.js
+++ postgresql.js
@@ -1,7 +1,7 @@
 const pgp = require('pg-promise')(/* options */)
 // const db = pgp(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT || 5432}/${process.env.DB_NAME}`)
 
-const db = pgp(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.oregon-postgres.render.com/${process.env.DB_NAME}?ssl=true`)
+const db = pgp(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.oregon-postgres.render.com/${process.env.DB_NAME_DEFAULT || process.env.DB_NAME}?ssl=true`)
 
 module.exports = db;
 
