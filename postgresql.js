4c3bf594ab728b63b1c49e873ca5b48e22b8056a
NgoMinhToan <toandev99@gmail.com>
2024-01-09 15:25:52 +0700

    change db connection string

--- postgresql.js
+++ postgresql.js
@@ -1,7 +1,7 @@
 const pgp = require('pg-promise')(/* options */)
 // const db = pgp(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT || 5432}/${process.env.DB_NAME}`)
 
-const db = pgp(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.oregon-postgres.render.com/${process.env.DB_NAME || process.env.DB_NAME_DEFAULT}?ssl=true`)
+const db = pgp(process.env.DB_CONNECTION_STRING || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.oregon-postgres.render.com/${process.env.DB_NAME || process.env.DB_NAME_DEFAULT}?ssl=true`)
 
 module.exports = db;
 
